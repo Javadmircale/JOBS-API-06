c1da2cb675e358b7974d4d4a5859678811725295
JavadMiracle <javad.miracle@gmail.com>
2024-03-09 14:22:57 +0330

    change 2

--- app.js
+++ app.js
@@ -26,11 +26,12 @@ app.use(cors());
 app.use(xss());
 
 // routers
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", authMiddleware, jobsRouter);
 app.get("/", (req, res) => {
   res.send("hello there");
 });
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", authMiddleware, jobsRouter);
+
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
